1ba60b9cc3cbe75944d5ed769208dca1cd630d6b
vedashreepatil28 <vedashree_patil28@milton.edu>
2025-12-08 20:32:22 -0500

    8/12/2025 Homework: Added more responses

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -14,16 +14,19 @@ public class Conditionals {
         int randomInt = (int)(Math.random()*10)+1;
         randomInt = (int) (Math.random()*10)+1;
         if(randomInt == 2 || randomInt == 4) {
-            System.out.println("Without a doubt!");
+            System.out.println("Obviously!");
         }
-        if(randomInt == 1 || randomInt == 5 || randomInt == 9) {
-            System.out.println("Better not tell you now!");
+        if(randomInt == 1 || randomInt == 5 ) {
+            System.out.println("Possibly...");
         }
         if(randomInt == 7 || randomInt == 10) {
-            System.out.println("Very doubtful!");
+            System.out.println("Nah...");
         }
-        if(randomInt == 3 || randomInt == 8 || randomInt == 6) {
-            System.out.println("As I see it, yes!");
+        if(randomInt == 3 || randomInt == 8 ) {
+            System.out.println("Seems like it");
+        }
+        if( randomInt == 9 || randomInt == 6) {
+            System.out.println("Sometimes!");
         }
 
         question = "Will it snow tomorrow? ";
@@ -34,14 +37,17 @@ public class Conditionals {
         if(randomInt2 == 2 || randomInt2 == 4) {
             System.out.println("Without a doubt!");
         }
-        if(randomInt2 == 1 || randomInt2 == 5 || randomInt2 == 9) {
-            System.out.println("Better not tell you now!");
+        if(randomInt2 == 1 || randomInt2 == 5 ) {
+            System.out.println("Ask again later!");
         }
         if(randomInt2 == 7 || randomInt2 == 10) {
             System.out.println("Very doubtful!");
         }
         if(randomInt2 == 3 || randomInt2 == 8 || randomInt2 == 6) {
-            System.out.println("As I see it, yes!");
+            System.out.println("Most likely");
+        }
+        if( randomInt2 == 9 || randomInt2 == 6) {
+            System.out.println("Maybe...");
         }
     }//constructor method
 
